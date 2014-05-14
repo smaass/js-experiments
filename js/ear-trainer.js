c22f8e3ab4c37bd9085af0d625cc71b256382bce
sergio <sergio.maass@gmail.com>
2014-05-14 18:14:16 -0400

    Added Chords

--- js/ear-trainer.js
+++ js/ear-trainer.js
@@ -1,10 +1,42 @@
-var velocity = 127;
-var delay = 0.8;
-var baseNote = 0;
-var interval = 0;
-var good = 0;
-var bad = 0;
+
 var midiLoaded = false;
+var chordTypes = {
+    min: [0, 3, 7],
+    maj: [0, 4, 7],
+    sus4: [0, 5, 7]
+};
+var intervalsData = IntervalsData();
+var chordsData = ChordsData();
+
+function IntervalsData() {
+    obj = GameData();
+    obj.interval = 0;
+    return obj;
+}
+
+function ChordsData() {
+    obj = GameData();
+    obj.delay = 1;
+    obj.chordKeys = Object.keys(chordTypes);
+    obj.randomChord = function() {
+        var k = obj.chordKeys;
+        obj.chordForm = k[ Math.floor(k.length * Math.random()) ];
+        obj.currentChord = chordTypes[obj.chordForm].map(function (n) {
+            return n + obj.baseNote;
+        });
+    }
+    return obj;
+}
+
+function GameData() {
+    return {
+        good: 0,
+        bad: 0,
+        velocity: 127,
+        delay: 0.8,
+        baseNote: 0
+    }
+}
 
 $(document).ready(function() {
     loadPlayMode();
@@ -47,48 +79,71 @@ function loadPlayMode() {
 }
 
 function startGame(name) {
-    if (name == 'practice') {
-        startPracticeGame();
+    if (name == 'intervals') {
+        startIntervalsGame();
     }
-    else {
-        startLearningGame();
+    else if (name == 'chords') {
+        startChordsGame();
     }
 }
 
-function startPracticeGame() {
-    activatePracticeListeners();
+function startIntervalsGame() {
+    activateIntervalButtons();
     nextInterval();
     $("#feedback").html("Select the interval");
-    updateMarker();
+    updateMarker(intervalsData);
 }
 
-function activatePracticeListeners() {
+function activateIntervalButtons() {
     $("#repeat").click(function() {
         playInterval();
     });
 
-    $(".button_interval").click(function() {
-        if (Math.abs(interval) == $(this).data("value")) {
+    $(".button_selection").click(function() {
+        if (Math.abs(intervalsData.interval) == $(this).data("value")) {
             $("#feedback").html("Good!");
-            good += 1;
+            intervalsData.good += 1;
             nextInterval();
         }
         else {
             $("#feedback").html("Wrong :(... try again");
-            bad += 1;
+            intervalsData.bad += 1;
             playInterval();
         }
-        updateMarker();
+        updateMarker(intervalsData);
     });
 }
 
-function startLearningGame() {
-
+function startChordsGame() {
+    activateChordButtons();
+    nextChord();
+    $("#feedback").html("Select the chord");
+    updateMarker(chordsData);
 }
 
-function updateMarker() {
-    $("#marker .good").html(good);
-	$("#marker .bad").html(bad);
+function activateChordButtons() {
+    $("#repeat").click(function() {
+        playCurrentChord();
+    });
+
+    $(".button_selection").click(function() {
+        if (chordsData.chordForm == $(this).data("value")) {
+            $("#feedback").html("Good!");
+            chordsData.good += 1;
+            nextChord();
+        }
+        else {
+            $("#feedback").html("Wrong :(... try again");
+            chordsData.bad += 1;
+            playCurrentChord();
+        }
+        updateMarker(chordsData);
+    });
+}
+
+function updateMarker(gameData) {
+    $("#marker .good").html(gameData.good);
+	$("#marker .bad").html(gameData.bad);
 }
 
 function randInt(min, max) {
@@ -96,17 +151,33 @@ function randInt(min, max) {
 }
 
 function nextInterval() {
-    baseNote = randInt(40, 80);
-    interval = randInt(-14, 14); // 14 semitones. Up or down.
+    intervalsData.baseNote = randInt(40, 80);
+    intervalsData.interval = randInt(-14, 14); // 14 semitones. Up or down.
     playInterval();
 }
 
+function nextChord() {
+    chordsData.baseNote = randInt(40, 80);
+    chordsData.randomChord();
+    playChord(chordsData.currentChord, chordsData.delay);
+}
+
+function playCurrentChord() {
+    playChord(chordsData.currentChord, chordsData.delay);
+}
+
+function playChord(notes, duration) {
+    MIDI.chordOn(0, notes, chordsData.velocity, 0);
+    MIDI.chordOff(0, notes, duration);
+}
+
 function playNote(note, delay, duration) {
-    MIDI.noteOn(0, note, velocity, delay);
+    MIDI.noteOn(0, note, intervalsData.velocity, delay);
     MIDI.noteOff(0, note, delay + duration);
 }
 
 function playInterval() {
-    playNote(baseNote, 0, delay);
-    playNote(baseNote + interval, delay, delay);
+    var id = intervalsData;
+    playNote(id.baseNote, 0, id.delay);
+    playNote(id.baseNote + id.interval, id.delay, id.delay);
 }
